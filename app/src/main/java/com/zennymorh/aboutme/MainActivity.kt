58b42fd3ed3b5d7c7e2ac3348c6f06e7636e220c
Zainab Jimoh <jimohzainab4753@gmail.com>
2020-01-12 00:09:45 +0000

    Made an app that uses data binding to work with layouts.

---
+++ app/src/main/java/com/zennymorh/aboutme/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.zennymorh.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.zennymorh.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var myName: MyName = MyName("Zainab Jimoh")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.GONE
+        }
+        //To hide the keyboard, even though we can just tap the screen but udacity is extra, so..
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ app/src/main/java/com/zennymorh/aboutme/MyName.kt
@@ -0,0 +1,4 @@
+package com.zennymorh.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "") {
+}
